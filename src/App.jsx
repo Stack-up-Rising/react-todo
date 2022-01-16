89713c0ef997bf531818c4ce92b39db48470c132
Kazuki Fukuyama <79068610+webzero07100805@users.noreply.github.com>
2022-01-16 19:54:22 +0900

    Task deletion function

--- src/App.jsx
+++ src/App.jsx
@@ -21,8 +21,7 @@ export const App = () => {
 
 	// 追加ボタンにonClickの処理を割り当てる。
 	const onClickAdd = () => {
-
-    // テキストボックスが空の状態で追加ボタンを押した際に処理を走らせないようにする。
+		// テキストボックスが空の状態で追加ボタンを押した際に処理を走らせないようにする。
 		if (todoText === '') return;
 
 		// 入力された値(todoText)を未完了のリスト(incompleteTodos)に追加する。
@@ -34,6 +33,19 @@ export const App = () => {
 
 		// 追加後にテキストボックス内に値が残らないように空文字でリセットする処理。
 		setTodoText('');
+	};
+
+	// 削除ボタン押下した際の処理
+	const onClickDelete = (index) => {
+		// 未完了TODO(incompleteTodos)から値を消すために新たに定数を定義
+		const newTodos = [...incompleteTodos];
+
+		// splice(配列要素を1つだけ削除)を使用し、リストから削除する。
+		// 第一引数(何番目の要素か？)と第二引数(いくつ削除するか？)を渡す。
+		newTodos.splice(index, 1);
+
+		// 未完了TODOをセットする関数(setIncompleteTodos)に先程定義したnewTodosを渡す。
+		setIncompleteTodos(newTodos);
 	};
 
 	return (
@@ -49,12 +61,13 @@ export const App = () => {
 			<div className='incomplete-area'>
 				<p className='title'>未完了のTODO</p>
 				<ul>
-					{incompleteTodos.map((todo) => {
+					{incompleteTodos.map((todo, index) => {
 						return (
 							<div key={todo} className='list-row'>
 								<li>{todo}</li>
 								<button>完了</button>
-								<button>削除</button>
+								{/* アロー関数で表示しないと常に削除の処理を走らせてしまう。 */}
+								<button onClick={() => onClickDelete(index)}>削除</button>
 							</div>
 						);
 					})}
